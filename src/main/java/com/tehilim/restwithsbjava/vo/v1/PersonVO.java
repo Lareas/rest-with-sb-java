c50ebff3eb2677b274ad44596ae2dc94b84d2b9a
Luiz Fernando Areas <lfernandoareas@gmail.com>
2024-08-28 16:05:28 -0300

    impl json serialization PersonVO

--- src/main/java/com/tehilim/restwithsbjava/vo/v1/PersonVO.java
+++ src/main/java/com/tehilim/restwithsbjava/vo/v1/PersonVO.java
@@ -1,16 +1,25 @@
 package com.tehilim.restwithsbjava.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@JsonPropertyOrder({"id", "address", "first_name", "lastName",  "gender"})
 public class PersonVO {
 
     private static final long serialVersionUID = 1L;
 
     private Long id;
+
+    @JsonProperty("first_name")
     private String firstName;
     private String lastName;
     private String address;
+
+    @JsonIgnore
     private String gender;
 
     public PersonVO() {
---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Roma - Italy', 'Clay', 'Male', 'Regazoni'),
+	(2, 'São Paulo - SP', 'Ayrton', 'Male', 'Senna'),
+	(5, 'Imola - Italy', 'Gian', 'Male', 'Francesco'),
+	(6, 'Paris - France', 'Natu', 'Female', 'July'),
+	(8, 'Paris - France', 'Nokola', 'Male', 'Tesla');
